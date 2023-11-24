da007a97aa37e94a7b39cc7cd0d20492d8afc463
bohdan <bohdanluben@gmail.com>
2023-11-24 15:48:33 +0200

    remove unused

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,8 +1,5 @@
 var express = require('express');
 var router = express.Router();
-const jwtProvider = require('jsonwebtoken');
-const jwtUtil = require('../util/jwtUtil');
-const {User} = require('../models/user');
 const controller = require('../controllers/adminController');
 
 router.get('/', controller.renderIndex);
